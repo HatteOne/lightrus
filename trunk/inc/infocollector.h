2e6b56a89c6d0f5d6bc8ae0e74d9ebb97fd202cb
bighatte <bighatte@635696aa-491d-11de-ba0e-b5da6ac7b600>
2009-05-26 09:02:18 +0000

    1) Added GNU headers to all source files 2) Corrections in russian dictionary file before line 1400

--- trunk/inc/infocollector.h
+++ trunk/inc/infocollector.h
@@ -1,8 +1,24 @@
 /*******************************************************************************
-  Функции для получения различной информации
 
-  Автор: Балякин С.В.
-  Дата: 28.04.2009
+    Project Lightrus
+    
+    Информационные функции
+    
+    Copyright (C) 2009 Sergey Balyakin
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
 *******************************************************************************/
 
 #ifndef INFOCOLLECTOR_H
